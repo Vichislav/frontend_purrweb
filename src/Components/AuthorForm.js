04efca3e5e2d7bda221c77faccfcf105510411a5
Svalyan <you@example.com>
2023-02-15 13:43:20 +0600

    cross for email input work correct

--- src/Components/AuthorForm.js
+++ src/Components/AuthorForm.js
@@ -2,6 +2,8 @@
 import '../css/AuthorForm.css'
 import { Link } from 'react-router-dom'
 import { useForm } from "react-hook-form";
+import cross from '../Assets/cross.svg'
+import purrweb_logo from "../Assets/purrweb_logo.svg";
 
 
 function AuthorForm() {
@@ -9,17 +11,31 @@ function AuthorForm() {
     const {
         register, /*позволяет регистрировать поля для формы*/
         formState: {
-            errors, isValid,
+            errors, isValid, isDirty,
         },
         handleSubmit, /*пропускает метод (onSubmit) только если нет errors*/
         reset, /*метод очистки формы после отправки*/
+        resetField,
     } = useForm({
         mode: "onBlur" /*проверяет инпут при смене фокуса*/
     });
 
+    const showEmailCross = () => {
+        document.getElementById('crossEmail').classList.remove('cross_Block')
+    }
+
+    const clearEmail = () => {
+        resetField('email')
+    }
+
+    const test = () => {
+        console.log('test work')
+    }
+
     const onSubmit = (data) => {
         alert(JSON.stringify(data))
         reset();
+        document.getElementById('crossEmail').classList.add('cross_Block')
     }
 
     return (
@@ -36,15 +52,23 @@ function AuthorForm() {
                       )}
                              type="text" className="wrap__Container_Middle_Input"
                       placeholder="example@mail.ru"/>
+                      <div id="crossEmail" className="wrap__Container_Middle_Cross cross_Block" onClick={clearEmail}>
+                          <img
+                              className="wrap__Container_Middle_img"
+                              src={cross}
+                              alt="cross not found"
+                          />
+                      </div>
                   </label>
-                  <div>
-                      {errors?.email && <p>{errors?.email?.message || "email not correct"}</p>}
-                  </div>
                   <label className="wrap__Container_Middle_Label_Password">
                       Пароль
                       <input type="text" className="wrap__Container_Middle_Password_Input"
                              placeholder="введите 8 значный пароль"/>
                   </label>
+                  <div className="wrap__Container_Middle_Error">
+                      {errors?.email && <p className="wrap__Container_Middle_Error_text">{errors?.email?.message || "Неверная почта или пароль"}</p>}
+                      {errors?.email && showEmailCross()}
+                  </div>
               </div>
               <div className="wrap__Container_Bottom">
                   <button type="submit"  className="wrap__Container_Bottom_Btn" disabled={!isValid}>
